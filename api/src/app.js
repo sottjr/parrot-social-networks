798fc87e7bcf90eb9f8728d330a2cbeaf9f734ec
Julio H Moura <juliohmoura@pm.me>
2022-06-16 18:09:57 -0300

    Update app.js

--- api/src/app.js
+++ api/src/app.js
@@ -7,4 +7,4 @@ app.use(express.json())
 db.hasConection();
 app.use(routes)
 
-app.listen(4000, () => console.log("Server running on port 3000"));
+app.listen(3000, () => console.log("Server running on port 3000"));
