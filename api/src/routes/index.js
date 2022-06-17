ef6d71729d86744a3847a84a006af5bf3565b7d9
Junior Sott <sottjr@gmail.com>
2022-06-17 10:08:14 +0200

    [fix] rotas

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -6,6 +6,13 @@ const AuthController = require("../controllers/authController")
 
 routes.post("/cadastrar", UsuarioController.create)
 
-routes.post("/CadUser",UsuarioController.create )
+routes.post("/login", AuthController.login)
+
+routes.put("/atualizar/:user_id", UsuarioController.update)
+
+routes.get("/getOne/:user_id", UsuarioController.getOne)
+routes.get("/listar", UsuarioController.getAll)
+
+routes.delete("/deletar/usuario/:user_id", UsuarioController.delete)
 
 module.exports = routes
